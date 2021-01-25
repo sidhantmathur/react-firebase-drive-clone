e0ad4ad49a06fd8dccc334e333f156aec72d4a74
sidhantmathur <sidhant185@gmail.com>
2021-01-24 19:23:09 -0600

    setup routes with react-router-dom and add login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import SignUp from "./components/SignUp"
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from './contexts/AuthContext'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import LogIn from './components/LogIn'
+
 
 function App() {
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px"}}>
-          <SignUp />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+      <div className="w-100" style={{ maxWidth: "400px"}}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path='/' component={Dashboard} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/login" component={LogIn} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
