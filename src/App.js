71468fef0adce42769665eb4ac5368986785fb5d
sidhantmathur <sidhant185@gmail.com>
2021-01-24 18:20:29 -0600

    responsive card

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Container } from 'react-bootstrap'
 function App() {
   return (
     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-      <SignUp />
+      <div className="w-100" style={{ maxWidth: "400px"}}>
+        <SignUp />
+      </div>
     </Container>
   );
 }
