a8f85acdb65b3ccd458f4deefb4317d2c5bd5532
sidhantmathur <sidhant185@gmail.com>
2021-02-02 15:14:50 -0600

    fix imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import SignUp from "./components/auth/SignUp"
 import { AuthProvider } from './contexts/AuthContext'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Profile from './components/Profile'
+import Profile from './components/auth/Profile'
 import LogIn from './components/auth/LogIn'
 import PrivateRoute from './components/auth/PrivateRoute'
 import ForgotPassword from './components/auth/ForgotPassword'
